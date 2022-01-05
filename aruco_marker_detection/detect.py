4afbe65b328abfe01990f20e50f6b29a632579c1
Will Martin <wcmartin01@gmail.com>
2022-01-04 21:20:55 -0600

    lower quality img and id markers

--- aruco_marker_detection/detect.py
+++ aruco_marker_detection/detect.py
@@ -1,5 +1,5 @@
 # detect ArUco Marker using OpenCV
-# https://mecaruco2.readthedocs.io/en/latest/notebooks_rst/Aruco/aruco_basics.html
+# source: https://mecaruco2.readthedocs.io/en/latest/notebooks_rst/Aruco/aruco_basics.html
 
 import numpy as np
 import cv2, PIL
@@ -22,7 +22,25 @@ def generate_markers():
 
     plt.show()
 
+def identify_markers():
+    frame = cv2.imread("imgs/img_1.png")
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+    parameters =  aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray_frame, aruco_dict, parameters=parameters)
+    frame_markers = aruco.drawDetectedMarkers(rgb_frame.copy(), corners, ids)
+
+    plt.figure()
+    plt.imshow(frame_markers)
+    for i in range(len(ids)):
+        c = corners[i][0]
+        plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label = "id={0}".format(ids[i]))
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
-    generate_markers()
+    # generate_markers()
+    identify_markers()
