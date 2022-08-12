cdfedc78ebb3acf868094397e3e05535b22d9d80
amukundan <amukundan@uiowa.edu>
2022-08-12 11:27:47 -0600

    working

--- aruco_marker_detection/positioning/aruco_pose_estimator.py
+++ aruco_marker_detection/positioning/aruco_pose_estimator.py
@@ -20,27 +20,11 @@ import pyautogui
 # Python version: 3.8
 
 # Dictionary that was used to generate the ArUco marker
-aruco_dictionary_name = "DICT_ARUCO_ORIGINAL"
+aruco_dictionary_name = "DICT_4X4_50"
 
 # The different ArUco dictionaries built into the OpenCV library.
 ARUCO_DICT = {
   "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-  "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-  "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-  "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-  "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-  "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-  "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-  "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-  "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-  "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-  "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-  "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-  "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-  "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-  "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-  "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-  "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL
 }
 
 # Side length of the ArUco marker in meters
@@ -104,7 +88,7 @@ def main():
   # Load the ArUco dictionary
   print("[INFO] detecting '{}' markers...".format(
     aruco_dictionary_name))
-  this_aruco_dictionary = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_dictionary_name])
+  this_aruco_dictionary = cv2.aruco.Dictionary_get(ARUCO_DICT["DICT_4X4_50"])
   this_aruco_parameters = cv2.aruco.DetectorParameters_create()
 
   # Start the video stream
@@ -141,6 +125,7 @@ def main():
       # Draw a square around detected markers in the video frame
     
       cv2.aruco.drawDetectedMarkers(frame, corners, marker_ids)
+      print(marker_ids)
 
       # Get the rotation and translation vectors
       # print('test1')
